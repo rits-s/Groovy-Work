11d4bf0335654b9d1053255bea3bc8383ccbe665
rits-s <79441429+rits-s@users.noreply.github.com>
2021-09-12 13:48:44 +1000

    first commit

---
+++ Groovy1/src/gpkg/testg.groovy
@@ -0,0 +1,14 @@
+package gpkg
+
+import static org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+class testg {
+
+	@Test
+	void test() {
+		fail("Not yet implemented")
+	}
+
+}
